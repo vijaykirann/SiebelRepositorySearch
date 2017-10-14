1d16e0e98e176d23cf41629d7635dedab3eef954
vijaykirann <Vicky@99>
2017-10-14 17:47:06 +0530

    workingconn

--- SiebelRepositorySearch/Form1.Designer.cs
+++ SiebelRepositorySearch/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtCN = new System.Windows.Forms.TextBox();
+            this.txtUN = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCN
+            // 
+            this.txtCN.Location = new System.Drawing.Point(126, 15);
+            this.txtCN.Name = "txtCN";
+            this.txtCN.Size = new System.Drawing.Size(202, 20);
+            this.txtCN.TabIndex = 0;
+            // 
+            // txtUN
+            // 
+            this.txtUN.Location = new System.Drawing.Point(126, 58);
+            this.txtUN.Name = "txtUN";
+            this.txtUN.Size = new System.Drawing.Size(202, 20);
+            this.txtUN.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(202, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ConnectString";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 280);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtUN);
+            this.Controls.Add(this.txtCN);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCN;
+        private System.Windows.Forms.TextBox txtUN;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SiebelRepositorySearch/Form1.cs
+++ SiebelRepositorySearch/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Odbc;
 
 namespace SiebelRepositorySearch
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OdbcConnection conn = new OdbcConnection("DSN=SSD Local Db default instance");
+           // conn.ConnectionString = "Driver={Oracle ODBC Driver};Dbq=SSD Local Db default instance;Uid=SADMIN;Pwd=SADMIN;";
+            conn.Open();
+            OdbcCommand dbCmd = conn.CreateCommand();
+            dbCmd.CommandText = "SELECT * FROM SIEBEL.S_ORG_EXT";
+            OdbcDataReader dbReader = dbCmd.ExecuteReader();
+            int fCount = dbReader.FieldCount;
+            Console.Write(":");
+            for (int i = 0; i < fCount; i++)
+            {
+                String fName = dbReader.GetName(i);
+                Console.Write(fName + ":");
+            }
+            Console.WriteLine();
+
         }
     }
 }
