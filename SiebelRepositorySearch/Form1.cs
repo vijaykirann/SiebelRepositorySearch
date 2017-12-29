65720cef3f35ba3b55f6b7f3bc54637ba1a409e2
vijay.nadella <vijay.chowdary99@gmail.com>
2017-12-29 16:41:17 +0530

    TestData

--- SiebelRepositorySearch/Form1.cs
+++ SiebelRepositorySearch/Form1.cs
@@ -37,6 +37,11 @@ namespace SiebelRepositorySearch
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            TestData testdata = new TestData();
+            testdata.data();
+            this.ResultListView.SetObjects(resultlist);
+            this.collapseGroup();
+
         }
 
         public void button1_Click(object sender, EventArgs ex)
@@ -102,26 +107,6 @@ namespace SiebelRepositorySearch
                     conn.Close();
                     toolStripStatusLabel1.Text = "Done";
                     statusStrip1.Refresh();
-
-
-                //    foreach (OLVColumn col in this.ResultListView.Columns)
-                //    {
-                //        object GrpKey = col.GetGroupKey(this.ResultListView.Groups);
-                //        col.MakeGroupies(
-                //new object[] { "Applet Control User Property", "Applet User Property Value", "Applet Server Script", "Applet Search Spec" },
-                //new string[] { "a", "b", "c", "d", "e" },
-                //new string[] { "", "hamburger", "toast", "beef", "chef" },
-                //new string[] {
-                //    "Pay good money -- or flee the house -- rather than eat their home-cooked food",
-                //    "Offer to buy takeaway rather than risk what may appear on your plate",
-                //    "Neither spectacular nor dangerous",
-                //    "Try to visit at dinner time to wrangle an invitation to dinner",
-                //    "Do whatever is necessary to procure their services"
-                //},
-                //new string[] { "Call 911", "Phone PizzaHut", "", "Open calendar", "Check bank balance" }
-                //            );
-                //    }
-            
                     this.ResultListView.SetObjects(resultlist);
                     this.collapseGroup();
 
---
+++ SiebelRepositorySearch/TestData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiebelRepositorySearch
+{
+    public class TestData
+    {
+        internal void data()
+        {
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "one", "two", "three", "four", "five"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "S", "G", "D", "I", "J"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "V", "M", "X", "R", "H"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "L", "Y", "C", "M", "H"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "L", "Z", "Y", "U", "Y"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "O", "Z", "Y", "T", "G"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "Q", "A", "H", "O", "D"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "Y", "M", "A", "E", "M"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "Y", "S", "A", "W", "K"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "E", "B", "U", "L", "C"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet User Property Value", "M", "U", "D", "A", "T"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Control User Property", "I", "S", "O", "O", "X"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Control User Property", "J", "N", "V", "Q", "I"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Control User Property", "M", "M", "P", "W", "T"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Control User Property", "M", "Z", "T", "U", "Z"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Control User Property", "L", "Q", "G", "X", "Y"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Control User Property", "A", "P", "N", "R", "O"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Control User Property", "Q", "K", "J", "V", "B"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Control User Property", "N", "E", "Z", "B", "K"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Control User Property", "N", "U", "B", "R", "R"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Dynamic Drilldown Destination", "Z", "Z", "E", "Q", "Q"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Dynamic Drilldown Destination", "O", "D", "N", "E", "K"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Dynamic Drilldown Destination", "H", "C", "G", "I", "R"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Dynamic Drilldown Destination", "W", "N", "L", "M", "L"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Browser Script", "B", "I", "M", "U", "U"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Browser Script", "N", "K", "M", "Z", "R"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Browser Script", "H", "A", "D", "D", "L"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Browser Script", "I", "N", "L", "T", "L"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Browser Script", "L", "Z", "D", "D", "T"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Browser Script", "H", "L", "Q", "R", "Q"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Browser Script", "A", "S", "D", "O", "W"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Browser Script", "F", "I", "F", "Q", "N"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Browser Script", "Q", "R", "W", "M", "I"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Server Script", "Q", "A", "I", "K", "A"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Server Script", "L", "Y", "Y", "Y", "T"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Server Script", "L", "J", "M", "G", "M"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Server Script", "P", "Q", "T", "O", "F"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Server Script", "I", "X", "W", "S", "H"));
+            SiebelRepositorySearch.resultlist.Add(new result("Applet Server Script", "P", "K", "W", "U", "H"));
+        }
+    }
+}
